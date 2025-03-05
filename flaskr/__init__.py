e04d4d31b2ba35953b78ee0db8ca676140c8af24
Janet <jhwilks@hotmail.com>
2025-03-05 08:07:07 +0000

    config for api added

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -12,6 +12,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY=os.getenv('SECRET_KEY'),
+        API_KEY=os.getenv('API_KEY'),
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
