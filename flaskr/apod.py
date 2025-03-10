9a4f0d5c609f04eacd58fb91c22f4a2c2ed9cb78
Janet <jhwilks@hotmail.com>
2025-03-10 18:11:21 +0000

    apod and gallery vertical slider working. Set up of gallery.html page ready to move gallery into it

--- flaskr/apod.py
+++ flaskr/apod.py
@@ -14,13 +14,20 @@ bp = Blueprint('apod', __name__)
 def apod():
     try:
         api_key = current_app.config['API_KEY']
-        url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
-        response = requests.get(url)
-        if response.status_code == 200:
-            apod_image = response.json()
-            return render_template('apod/apod_index.html', image_url=apod_image['url'], title=apod_image['title'])
+        url_gallery = f"https://api.nasa.gov/planetary/apod?count=10&api_key={api_key}"
+        url_apod = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
+        response_gallery = requests.get(url_gallery)
+        response_apod = requests.get(url_apod)
+        if response_gallery.status_code == 200 & response_apod.status_code == 200:
+            apod_today = response_apod.json()
+            apod_today_image = apod_today['url']
+            apod_today_title = apod_today['title']
+            apod_images = response_gallery.json()
+            image_urls = [image['url'] for image in apod_images]
+            titles = [image['title'] for image in apod_images]
+            return render_template('apod/apod_index.html', image_urls=image_urls, titles=titles, arr_len=len(titles), apod_today_image=apod_today_image, apod_today_title=apod_today_title)
         else:
-            return f"Error - unable to fetch data. {response.status_code}", 500
+            return f"Error - unable to fetch data. {response_gallery.status_code}, {response_apod.status_code}", 500
     except Exception as e:
         return f"Error occurred: {e}", 500
 
