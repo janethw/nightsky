b1ea7a473d1afb1fdc72ea3e0fdb3318fc78dc86
Janet <jhwilks@hotmail.com>
2025-03-22 21:17:33 +0000

    transitioned auth.py to sqlalchemy2

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -62,12 +62,12 @@ def login():
 
         if user is None:
             error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
+        elif not check_password_hash(user.password, password):
             error = 'Incorrect password.'
 
         if error is None:
             session.clear()
-            session['user_id'] = user['id']
+            session['user_id'] = user.id
             return redirect(url_for('index'))
 
         flash(error)
@@ -82,10 +82,10 @@ def load_logged_in_user():
     if user_id is None:
         g.user = None
     else:
-        g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?', (user_id,)
-        ).fetchone()
-
+        # g.user = get_db().execute(
+        #     'SELECT * FROM user WHERE id = ?', (user_id,)
+        # ).fetchone()
+        g.user = db.session.execute(db.select(User).where(User.id == user_id).scalar_one_or_none())
 
 @bp.route('/logout')
 def logout():
